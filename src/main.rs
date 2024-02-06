c24c2f2d0fcfeae706f6715c6ebd5d296e14cee6
kxie <kxie@ualberta.ca>
2024-02-06 11:09:17 +0800

    change to bevy, add shape

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+
+#[derive(Component)]
+struct MyCameraMarker;
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn((
+        Camera2dBundle {
+            transform: Transform::from_xyz(100.0, 200.0, 0.0),
+            ..default()
+        },
+        MyCameraMarker,
+    ));
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    // Circle
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+        material: materials.add(ColorMaterial::from(Color::PURPLE)),
+        transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
+        ..default()
+    });
+}
+
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (setup_camera, setup))
+        .run();
+}
