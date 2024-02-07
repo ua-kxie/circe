a5c3a30cbff5e54a175884e303eac4f4ef986c0d
kxie <kxie@ualberta.ca>
2024-02-07 11:29:41 +0800

    add zoom

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::ops::Mul;
 
-use bevy::{input::mouse::MouseMotion, prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::{input::mouse::MouseWheel, prelude::*, sprite::MaterialMesh2dBundle};
 
 #[derive(Component)]
 struct MyCameraMarker;
@@ -24,24 +24,30 @@ fn setup(
     commands.spawn(MaterialMesh2dBundle {
         mesh: meshes.add(shape::Circle::new(50.)).into(),
         material: materials.add(ColorMaterial::from(Color::PURPLE)),
-        transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
+        transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
         ..default()
     });
 }
 
 fn camera_transform(
     mb: Res<ButtonInput<MouseButton>>,
-    mut mm: EventReader<MouseMotion>,
+    mut mm: EventReader<CursorMoved>,
+    mut mw: EventReader<MouseWheel>,
     mut camera: Query<(&mut Transform, &MyCameraMarker)>
 ) {
-    if mb.pressed(MouseButton::Middle) {
-        if let Ok(mut a) = camera.get_single_mut() {
+    if let Ok(mut a) = camera.get_single_mut() {
+        if mb.pressed(MouseButton::Middle) {
             let mut pan = Vec3::ZERO;
             for m in mm.read() {
-                pan += Vec3::new(-m.delta.x, m.delta.y, 0.0);
+                if let Some(d) = m.delta {
+                    pan += Vec3::new(-d.x, d.y, 0.0);
+                }
             }
             let t = a.0.scale.mul(pan);
             a.0.translation += t;
+        }
+        for mwe in mw.read() {
+            a.0.scale *= 1. - mwe.y / 10.
         }
     }
 }
