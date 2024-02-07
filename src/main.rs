85bf91bfda9246a72f64be1b0b893ed59cb0b605
kxie <kxie@ualberta.ca>
2024-02-07 17:12:26 +0800

    cargo fmt, add to schedule

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-use std::ops::Mul;
 use bevy::window::PrimaryWindow;
 use bevy::{input::mouse::MouseWheel, prelude::*, sprite::MaterialMesh2dBundle};
 use euclid::{Box2D, Point2D};
+use std::ops::Mul;
 
 /// PhantomData tag used to denote the i16 space in which the schematic exists
-#[derive(
-    Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash,
-)]
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct WorldSpace;
 
 #[derive(Component)]
@@ -29,8 +27,10 @@ fn cursor_to_world(
 ) {
     if let Ok((camera, cam_transform)) = q_camera.get_single() {
         if let Ok(window) = q_window.get_single() {
-            if let Some(coords) = window.cursor_position()
-            .and_then(|cursor| camera.viewport_to_world_2d(cam_transform, cursor)) {
+            if let Some(coords) = window
+                .cursor_position()
+                .and_then(|cursor| camera.viewport_to_world_2d(cam_transform, cursor))
+            {
                 schematic_coords.0 = coords;
             }
         }
@@ -54,17 +54,17 @@ fn window_to_world(
                 Vec2::new(0., window.height()),
                 Vec2::new(window.width(), window.height()),
             ];
-            let bb = corners.iter().filter_map(|p| camera.viewport_to_world_2d(cam_transform, *p).map(|v| Point2D::new(v.x, v.y)));
+            let bb = corners.iter().filter_map(|p| {
+                camera
+                    .viewport_to_world_2d(cam_transform, *p)
+                    .map(|v| Point2D::new(v.x, v.y))
+            });
             // .map(|p| camera.viewport_to_world_2d(cam_transform, p));
-        visible_coords.0 = Some(Box2D::from_points(bb));
-        return
+            visible_coords.0 = Some(Box2D::from_points(bb));
+            return;
         }
     }
-    visible_coords.0 = None  // if theres any problem, assume camera doesnt see anything
-}
-
-fn bounding_box(pts: Vec<euclid::Point2D<f32, WorldSpace>>) -> Box2D<f32, WorldSpace> {
-    Box2D::from_points(pts)
+    visible_coords.0 = None // if theres any problem, assume camera doesnt see anything
 }
 
 fn setup_camera(mut commands: Commands) {
@@ -119,5 +119,6 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (setup_camera, setup))
         .add_systems(Update, camera_transform)
+        .add_systems(Update, window_to_world)
         .run();
 }
