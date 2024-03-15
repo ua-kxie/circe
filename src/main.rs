782094b0b5bf65abe0b2ec9b171e2a4a356b9796
kxie <kxie@ualberta.ca>
2024-03-15 14:49:39 +0800

    hide cursor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
 
 mod schematic;
 mod types;
@@ -6,5 +6,12 @@ mod types;
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, schematic::SchematicPlugin))
+        .add_systems(Startup, hide_cursor)
         .run();
+}
+pub fn hide_cursor(
+    mut primary_window: Query<&mut Window, With<PrimaryWindow>>,
+) {
+    let window = &mut primary_window.single_mut();
+    window.cursor.visible = false;
 }
