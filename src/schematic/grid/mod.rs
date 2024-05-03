2b733d3c66c09feca5f8152380be865954b0a2cf
ua-kxie <kxie@ualberta.ca>
2024-05-03 17:56:23 +0800

    major and minor grids

--- src/schematic/grid/mod.rs
+++ src/schematic/grid/mod.rs
@@ -16,17 +16,26 @@ impl Plugin for Grid {
     fn build(&self, app: &mut App) {
         app.add_plugins(MaterialPlugin::<GridMaterial>::default());
         app.add_systems(Startup, setup);
-        app.add_systems(Update, main);
+        app.add_systems(Update, (major_grid_main, minor_grid_main));
     }
 }
 
 #[derive(Component)]
-struct GridMarker;
+struct MinorGridMarker;
+
+#[derive(Component)]
+struct MajorGridMarker;
 
 #[derive(Bundle)]
-struct GridBundle {
+struct MinorGridBundle {
     mesh: MaterialMeshBundle<ui::GridMaterial>,
-    grid: GridMarker,
+    grid: MinorGridMarker,
+}
+
+#[derive(Bundle)]
+struct MajorGridBundle {
+    mesh: MaterialMeshBundle<ui::GridMaterial>,
+    grid: MajorGridMarker,
 }
 
 fn setup(
@@ -35,10 +44,10 @@ fn setup(
     mut grid_materials: ResMut<Assets<ui::GridMaterial>>,
 ) {
     // grid
-    commands.spawn(GridBundle {
+    commands.spawn(MinorGridBundle {
         mesh: MaterialMeshBundle {
             material: grid_materials.add(ui::GridMaterial {
-                color: Color::WHITE,
+                color: Color::rgba(0.5, 0.5, 0.5, 0.5),
             }),
             mesh: meshes
                 .add(Mesh::new(
@@ -48,7 +57,24 @@ fn setup(
                 .into(),
             ..default()
         },
-        grid: GridMarker,
+        grid: MinorGridMarker,
+    });
+
+    // grid
+    commands.spawn(MajorGridBundle {
+        mesh: MaterialMeshBundle {
+            material: grid_materials.add(ui::GridMaterial {
+                color: Color::rgba(1.0, 1.0, 1.0, 1.0),
+            }),
+            mesh: meshes
+                .add(Mesh::new(
+                    PrimitiveTopology::PointList,
+                    RenderAssetUsages::RENDER_WORLD | RenderAssetUsages::MAIN_WORLD,
+                ))
+                .into(),
+            ..default()
+        },
+        grid: MajorGridMarker,
     });
 
     // axis
@@ -76,8 +102,8 @@ fn setup(
 }
 
 // place grid dots according to visible canvas aabb
-fn main(
-    mut g: Query<(Entity, &mut Handle<Mesh>), With<GridMarker>>,
+fn minor_grid_main(
+    mut g: Query<(Entity, &mut Handle<Mesh>), With<MinorGridMarker>>,
     mut e_new_viewport: EventReader<NewVisibleCanvasAABB>,
     schematic_res: ResMut<SchematicRes>,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -85,33 +111,48 @@ fn main(
 ) {
     if let Some(_) = e_new_viewport.read().last() {
         let aabb = schematic_res.visible_canvas_aabb.0.unwrap();
+
         let grid = g.get_single_mut().unwrap();
         let gridmesh = meshes.get_mut(grid.1.id()).unwrap();
-
-        // delete old grid
         gridmesh.remove_attribute(Mesh::ATTRIBUTE_POSITION);
-        
-        gridmesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, gridvec(aabb));
-
+        gridmesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, gridvec(aabb, 2, 1.0e4));
         if let Some(aabb) = gridmesh.compute_aabb() {
             commands.entity(grid.0).insert(aabb);
         }
+
+    }
+}
+
+// place grid dots according to visible canvas aabb
+fn major_grid_main(
+    mut g: Query<(Entity, &mut Handle<Mesh>), With<MajorGridMarker>>,
+    mut e_new_viewport: EventReader<NewVisibleCanvasAABB>,
+    schematic_res: ResMut<SchematicRes>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut commands: Commands,
+) {
+    if let Some(_) = e_new_viewport.read().last() {
+        let aabb = schematic_res.visible_canvas_aabb.0.unwrap();
+
+        let grid = g.get_single_mut().unwrap();
+        let gridmesh = meshes.get_mut(grid.1.id()).unwrap();
+        gridmesh.remove_attribute(Mesh::ATTRIBUTE_POSITION);
+        gridmesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, gridvec(aabb, 16, 1.0e6));
+        if let Some(aabb) = gridmesh.compute_aabb() {
+            commands.entity(grid.0).insert(aabb);
+        }
+
     }
 }
 
 fn gridvec(
     aabb: Box2D<i32, SchematicSpace>,
+    spacing: i32,
+    area_limit: f32,
 ) -> Vec<Vec3> {
     let area = aabb.height() as f32 * aabb.width() as f32;
-    if area > 1000000.0 {
+    if area > area_limit {
         return vec![];
-    }
-
-    let spacing;
-    if area < 10000.0 {
-        spacing = 2;
-    } else {
-        spacing = 16;
     }
     let height = aabb.height() / spacing + 1;
     let width = aabb.width() / spacing + 1;
